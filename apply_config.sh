d056d24abee2e68d84b371d646f99d08ab420c12
radx64 <radekteam@gmail.com>
2018-10-30 20:34:32 +0100

    Initial configuration with systemd improvements for SPI being mounted too late

---
+++ apply_config.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cp config/hyperion.path /etc/systemd/system/hyperion.path
+cp config/hyperion.service /etc/systemd/system/hyperion.service
+cp config/hyperion.config.json /etc/hyperion/hyperion.config.json
